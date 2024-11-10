69c1215647a17d73228e82a9618886e1b573af2e
Samarth Narayan <kayaniv321@gmail.com>
2024-11-10 13:26:43 +0530

    bakend setup

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+// import authRoutes from "./routes/authRoutes.js";
+// import messageRoutes from "./routes/messageRoutes.js";
+// import userRoutes from "./routes/userRoutes.js";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+        .then(()=>{ console.log("Database connected!!"); })
+        .catch((err)=>{ console.log(err) }); 
+
+const PORT = process.env.PORT || 5000;
+
+const app=express();
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT, ()=>{
+    console.log('Server is running on port 5000!!');
+});
+
+app.get("/test",(req,res,next)=>{
+    return res.send("Hello");
+});

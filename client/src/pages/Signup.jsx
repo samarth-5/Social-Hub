3611ae85fdd96299ae1dddc8991b3b3907aac8f9
Samarth Narayan <kayaniv321@gmail.com>
2024-11-10 12:30:38 +0530

    completed signup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,128 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function Signup() {
+
+  const [inputs, setInputs] = useState({
+		fullName: "",
+		username: "",
+		password: "",
+		confirmPassword: "",
+		gender: "",
+	});
+
+  const GenderCheckbox = ({ onCheckboxChange, selectedGender }) => {
+    return (
+      <div className='flex'>
+        <div className='form-control'>
+          <label className={`label gap-2 cursor-pointer ${selectedGender === "male" ? "selected" : ""} `}>
+            <span className='label-text'>Male</span>
+            <input
+              type='checkbox'
+              className='checkbox border-slate-900'
+              checked={selectedGender === "male"}
+              onChange={() => onCheckboxChange("male")}
+            />
+          </label>
+        </div>
+        <div className='form-control'>
+          <label className={`label gap-2 cursor-pointer  ${selectedGender === "female" ? "selected" : ""}`}>
+            <span className='label-text'>Female</span>
+            <input
+              type='checkbox'
+              className='checkbox border-slate-900'
+              checked={selectedGender === "female"}
+              onChange={() => onCheckboxChange("female")}
+            />
+          </label>
+        </div>
+      </div>
+    );
+  };
+
+  const handleCheckboxChange = (gender) => {
+		setInputs({ ...inputs, gender });
+	};
+  
+  const handleSubmit=()=>{}
+
   return (
-    <div>Signup</div>
-  )
+		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+				<h1 className='text-3xl font-semibold text-center text-gray-300'>
+					Sign Up <span className='text-blue-500'> SOCIAL HUB</span>
+				</h1>
+
+				<form onSubmit={handleSubmit}>
+					<div>
+						<label className='label p-2'>
+							<span className='text-base label-text'>Full Name</span>
+						</label>
+						<input
+							type='text'
+							placeholder='John Doe'
+							className='w-full input input-bordered  h-10'
+							value={inputs.fullName}
+							onChange={(e) => setInputs({ ...inputs, fullName: e.target.value })}
+						/>
+					</div>
+
+					<div>
+						<label className='label p-2 '>
+							<span className='text-base label-text'>Username</span>
+						</label>
+						<input
+							type='text'
+							placeholder='johndoe'
+							className='w-full input input-bordered h-10'
+							value={inputs.username}
+							onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
+						/>
+					</div>
+
+					<div>
+						<label className='label'>
+							<span className='text-base label-text'>Password</span>
+						</label>
+						<input
+							type='password'
+							placeholder='Enter Password'
+							className='w-full input input-bordered h-10'
+							value={inputs.password}
+							onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
+						/>
+					</div>
+
+					<div>
+						<label className='label'>
+							<span className='text-base label-text'>Confirm Password</span>
+						</label>
+						<input
+							type='password'
+							placeholder='Confirm Password'
+							className='w-full input input-bordered h-10'
+							value={inputs.confirmPassword}
+							onChange={(e) => setInputs({ ...inputs, confirmPassword: e.target.value })}
+						/>
+					</div>
+
+					<GenderCheckbox onCheckboxChange={handleCheckboxChange} selectedGender={inputs.gender} />
+
+					<Link
+						to={"/login"}
+						className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'
+						href='#'
+					>
+						Already have an account?
+					</Link>
+
+					{/* <div>
+						<button className='btn btn-block btn-sm mt-2 border border-slate-700' disabled={loading}>
+							{loading ? <span className='loading loading-spinner'></span> : "Sign Up"}
+						</button>
+					</div> */}
+				</form>
+			</div>
+		</div>
+	);
 }

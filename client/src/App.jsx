ce89a587b86e85564c9cc43eb23034358c56c3b6
Samarth Narayan <kayaniv321@gmail.com>
2024-11-10 12:03:29 +0530

    created pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import Login from './pages/Login.jsx';
+import Home from './pages/Home.jsx';
+import Signup from './pages/Signup.jsx';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export default function App() {
   return (
-    <button class="btn btn-primary">Primary</button>
+    <div className='p-4 h-screen flex items-center justify-center'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+				  <Route path='/login' element={<Login />} />
+				  <Route path='/signup' element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }

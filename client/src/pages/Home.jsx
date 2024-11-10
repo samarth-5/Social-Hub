d5ddf487bb0b60156f815d4acf6736df86a5fe00
Samarth Narayan <kayaniv321@gmail.com>
2024-11-10 12:43:14 +0530

    created sidebar and message component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import Sidebar from "../components/Sidebar/Sidebar.jsx";
+import Message from "../components/Messages/Message.jsx";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+		<div className='flex sm:h-[450px] md:h-[620px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+			<Sidebar />
+			<Message />
+		</div>
+	);
 }

69c247926c8413a19d19c18ad84be22ed2ad7864
Samarth Narayan <kayaniv321@gmail.com>
2024-11-11 20:44:15 +0530

    convo selected color implemented

--- client/src/components/Sidebar/Conversations.jsx
+++ client/src/components/Sidebar/Conversations.jsx
@@ -1,14 +1,23 @@
 import Conversation from "./Conversation.jsx";
+import useGetConversations from "../../hooks/useGetConversation.jsx";
+import { getRandomEmoji } from "../../utils/emojis.js";
 
 export default function Conversations() {
-    return (
-        		<div className='py-2 flex flex-col overflow-auto'>
-        			<Conversation />
-        			<Conversation />
-        			<Conversation />
-        			<Conversation />
-        			<Conversation />
-        			<Conversation />
-        		</div>
-        	);
+
+    const { loading, conversations } = useGetConversations();
+
+	return (
+		<div className='py-2 flex flex-col overflow-auto'>
+			{conversations.map((conversation, idx) => (
+				<Conversation
+					key={conversation._id}
+					conversation={conversation}
+					emoji={getRandomEmoji()}
+					lastIdx={idx === conversations.length - 1}
+				/>
+			))}
+
+			{loading ? <span className='loading loading-spinner mx-auto'></span> : null}
+		</div>
+	);
 }

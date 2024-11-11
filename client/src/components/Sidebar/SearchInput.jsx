986bcffffe927bb34e2324388648f5411fe4c48b
Samarth Narayan <kayaniv321@gmail.com>
2024-11-12 01:05:52 +0530

    search functionlaity completed

--- client/src/components/Sidebar/SearchInput.jsx
+++ client/src/components/Sidebar/SearchInput.jsx
@@ -1,11 +1,29 @@
 import { IoSearchSharp } from "react-icons/io5";
 import { useState } from "react";
+import useConversation from "../../zustand/useConversation.js";
+import useGetConversations from "../../hooks/useGetConversation.jsx";
+import toast from "react-hot-toast";
 
 export default function SearchInput() {
 
     const [search, setSearch] = useState("");
+	const { setSelectedConversation } = useConversation();
+	const { conversations } = useGetConversations();
 
-    const handleSubmit=()=>{};
+    const handleSubmit=(e)=>{
+		e.preventDefault();
+		if (!search) return;
+		if (search.length < 3) {
+			return toast.error("Search term must be at least 3 characters long!");
+		}
+		const conversation = conversations.find((c) => c.fullName.toLowerCase().includes(search.toLowerCase()));
+		if (conversation) {
+			setSelectedConversation(conversation);
+			setSearch("");
+		} 
+		else 
+		toast.error("No such user found!");
+	};
 
     return (
 		<form onSubmit={handleSubmit} className='flex items-center gap-2'>

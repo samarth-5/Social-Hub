82d37e2ba3562368dd00d3766ea9a16fd045af6f
Samarth Narayan <kayaniv321@gmail.com>
2024-11-11 23:34:34 +0530

    sending and receiving messages

--- client/src/components/Messages/Messages.jsx
+++ client/src/components/Messages/Messages.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import { useEffect, useRef } from "react";
+import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeleton from "./MessageSkeleton.jsx";
+import Message from "./Message";
+//import useListenMessages from "../../hooks/useListenMessages";
 
 export default function Messages() {
-  return (
-    <div>Messages</div>
-  )
+  const { messages, loading } = useGetMessages();
+	//();
+	const lastMessageRef = useRef();
+
+	useEffect(() => {
+		setTimeout(() => {
+			lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+		}, 100);
+	}, [messages]);
+
+	return (
+		<div className='px-4 flex-1 overflow-auto'>
+			{!loading &&
+				messages.length > 0 &&
+				messages.map((message) => (
+					<div key={message._id} ref={lastMessageRef}>
+						<Message message={message} />
+					</div>
+				))}
+
+			{loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+			{!loading && messages.length === 0 && (
+				<p className='text-center'>Send a message to start the conversation</p>
+			)}
+		</div>
+	);
 }

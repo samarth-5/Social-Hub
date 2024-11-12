6e919d6e76d4a2c7307df441601806feee5d4488
Samarth Narayan <kayaniv321@gmail.com>
2024-11-12 11:29:42 +0530

    minor changes

--- client/src/components/Messages/MessageInput.jsx
+++ client/src/components/Messages/MessageInput.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { BsSend } from "react-icons/bs";
 import useSendMessage from "../../hooks/useSendMessage.jsx";
+import { FaRegSmileBeam } from "react-icons/fa";
 
 export default function MessageInput() {
     const [message, setMessage] = useState("");
@@ -16,10 +17,14 @@ export default function MessageInput() {
 	return (
 		<form className='px-4 my-3' onSubmit={handleSubmit}>
 			<div className='w-full relative'>
+			
+			    <button className='absolute inset-y-0 start-3 flex items-center pe-3'>
+				    <FaRegSmileBeam />
+				</button>				
 				<input
 					type='text'
 					className='border text-sm rounded-lg block w-full p-2.5  bg-gray-700 border-gray-600 text-white'
-					placeholder='Send a message'
+					placeholder='        Send a message'
 					value={message}
 					onChange={(e) => setMessage(e.target.value)}
 				/>

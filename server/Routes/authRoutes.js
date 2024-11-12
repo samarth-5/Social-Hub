0518d82dc9e541f009c6b6b0894cd4e7085d4ce1
Samarth Narayan <kayaniv321@gmail.com>
2024-11-12 19:37:13 +0530

    minor issue

--- server/Routes/authRoutes.js
+++ server/Routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/authController.js";
+import { login, logout, signup } from "../Controllers/authController.js";
 
 const router = express.Router();
 

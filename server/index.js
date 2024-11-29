58cbca6baa0d1badb346d236c23ce17adc990a82
Samarth Narayan <kayaniv321@gmail.com>
2024-11-29 18:44:05 +0530

    frontend url changed

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ mongoose.connect(process.env.MONGO_URL)
         .then(()=>{ console.log("Database connected!!"); })
         .catch((err)=>{ console.log(err) }); 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 import { app, server } from "./socket/socket.js";
 
@@ -25,7 +25,7 @@ app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
 server.listen(PORT, ()=>{
-    console.log('Server is running on port 5000!!');
+    console.log(`Server is running on port ${PORT}!!`);
 });
 
 app.get("/test",(req,res,next)=>{
--- server/socket/socket.js
+++ server/socket/socket.js
@@ -1,14 +1,17 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: ["https://social-hub-5.onrender.com"],
-		methods: ["GET", "POST"],
+		// methods: ["GET", "POST"],
+		origin: process.env.FRONTEND_URL,
+        credentials: true,
 	},
 });
 

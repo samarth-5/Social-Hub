a1fbdd29df914237dee000cddb0c310a173090de
Samarth Narayan <kayaniv321@gmail.com>
2024-11-29 19:34:49 +0530

    added footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ export default function App() {
   const { authUser } = useAuthContext();
 
   return (
+    <div className='flex flex-col h-screen'>
     <div className='p-4 h-screen flex items-center justify-center'>
         <Routes>
           <Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
@@ -18,6 +19,10 @@ export default function App() {
 				  <Route path='/signup' element={authUser ? <Navigate to='/' /> : <Signup />} />
         </Routes>
         <Toaster />
+    </div>
+    <div className='text-center text-black font-bold p-4'>
+      © 2024 Developed by @<a href='https://samarth-dev.onrender.com/' target="_blank" className="font-semibold hover:underline">Samarth</a>. All rights reserved.
+    </div>
     </div>
   )
 }
